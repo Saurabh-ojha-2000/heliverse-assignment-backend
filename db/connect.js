db325ecb4149c085c5e1eeac9edf185cd9c58dc1
Saurabh Ojha <107386248+Saurabh-ojha-2000@users.noreply.github.com>
2024-08-28 00:23:31 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -16,25 +16,3 @@ const connectDB = async () => {
 };
 
 export default connectDB;
-
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const connectDB = async () => {
-//   try {
-//     console.log('Connecting to MongoDB with URL:', process.env.MONGO_URL);
-//     await mongoose.connect(process.env.MONGO_URL, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('MongoDB connection SUCCESS');
-//   } catch (error) {
-//     console.error('MongoDB connection FAIL');
-//     console.error(error);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
